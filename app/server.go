0a7d2cfe9d2b164bbd02112a3e7c362d7a36e9a1
mr_a953@mail.ru <mr_a953@mail.ru>
2024-07-16 00:22:45 +0300

    test

--- app/server.go
+++ app/server.go
@@ -28,20 +28,36 @@ func main() {
 		fmt.Printf("Error reading: %#v\n", err)
 		return
 	}
-	words := strings.Fields(string(buf))
+	packet := strings.Fields(string(buf))
 
-	path := words[1]
+	path := packet[1]
 
-	words = strings.Split(path, "/")
-	if words[1] != "echo" {
-		if path == "/" {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if path == "/user-agent" {
+		msg := ""
+		for i := 3; i < len(packet); i++ {
+			fmt.Println(packet[i])
+			dict := strings.Split(packet[i], ":")
+			fmt.Println(dict)
+			if len(dict) != 2 {
+				continue
+			}
+			header, value := dict[0], dict[1]
+			fmt.Println(header, " ", value)
+			header = strings.ToLower(header)
+			if header == "user-agent" {
+				msg = value
+				break
+			}
 		}
-	} else {
-		msg := words[2]
+
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		msg := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 
 	conn.Close()
