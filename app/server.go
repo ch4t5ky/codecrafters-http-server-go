1cffa6a313367d5e8208baa7836a0177cfd4e56c
mr_a953@mail.ru <mr_a953@mail.ru>
2024-07-16 00:22:42 +0300

    test

--- app/server.go
+++ app/server.go
@@ -32,9 +32,8 @@ func main() {
 	if words[1] == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 	fmt.Printf("Message received: %s\n", string(buf[:len]))
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	conn.Close()
 }
