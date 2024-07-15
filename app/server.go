f6da58284e42a74a2f65220a7be8f3b4a0539ee8
mr_a953@mail.ru <mr_a953@mail.ru>
2024-07-16 00:22:48 +0300

    test

--- app/server.go
+++ app/server.go
@@ -24,26 +24,26 @@ func main() {
 			os.Exit(1)
 		}
 
-		HandleConnection(conn)
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Error reading: %#v\n", err)
+			return
+		}
+		response := HandleConnection(string(buf))
+		conn.Write(response)
+		conn.Close()
 	}
 }
 
-func HandleConnection(conn net.Conn) {
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Printf("Error reading: %#v\n", err)
-		return
-	}
-	packet := strings.Fields(string(buf))
-
-	path := packet[1]
-
+func HandleConnection(request string) []byte {
+	path := strings.Split(request, " ")[1]
+	response := []byte("")
 	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
 	} else if path == "/user-agent" {
 		msg := ""
-		packet = strings.Split(string(buf), "\r\n")
+		packet := strings.Split(request, "\r\n")
 		for i := 0; i < len(packet); i++ {
 			fmt.Println(packet[i])
 			dict := strings.Split(packet[i], ":")
@@ -58,14 +58,12 @@ func HandleConnection(conn net.Conn) {
 			}
 		}
 
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)))
+		response = []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg))
 	} else if strings.Split(path, "/")[1] == "echo" {
 		msg := strings.Split(path, "/")[2]
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)))
+		response = []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg))
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 	}
-
-	conn.Close()
-	return
+	return response
 }
